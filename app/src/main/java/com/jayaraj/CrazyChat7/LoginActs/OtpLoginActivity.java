04de65552b5cd5d1670573b06cf919fc7bd3296d
Mkjk007 <88058607+Mkjk007@users.noreply.github.com>
2023-01-27 13:58:24 +0530

    temp

--- app/src/main/java/com/jayaraj/CrazyChat7/LoginActs/OtpLoginActivity.java
+++ app/src/main/java/com/jayaraj/CrazyChat7/LoginActs/OtpLoginActivity.java
@@ -417,6 +417,7 @@ public class OtpLoginActivity extends AppCompatActivity {
     }
 
     public void _Anime(String _Sp, View _Linear) {
+        //Hello
         if ("".equals(this.asp.getString(_Sp, ""))) {
             this.asp.edit().putString(_Sp, "true").apply();
             final AutoTransition autoTransition = new AutoTransition();
